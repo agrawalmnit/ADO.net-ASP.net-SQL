ab2dd29d5c33d1eb22d430a91e798354f55b2e04
ishansi-abjima <119054363+ishansi-abjima@users.noreply.github.com>
2023-01-23 17:37:33 +0530

    Add files via upload

---
+++ WebApplication6/WebApplication6/Controllers/HomeController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication6.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getall")]
+        public List<Person> GetPersons()
+        {
+            PersonDB db = new PersonDB();
+
+            List<Person> obj = db.GetPersons();
+
+            return obj;
+        }
+
+
+        [HttpDelete]
+        [Route("delete")]
+        public bool DeletePerson(Person person, int PersonID)
+        {
+            PersonDB db = new PersonDB();
+
+            List<Person> obj2 = db.GetPersons();
+
+            var PersonToBeDeleted = (from s in obj2
+                                     where s.PersonID == PersonID
+                                     select s).FirstOrDefault();
+            obj2.Remove(PersonToBeDeleted);
+
+            return true;
+
+        }
+
+
+        [HttpPut]
+        [Route("put")]
+
+        public bool UpdatePerson(Person person, int PersonID)
+        {
+            PersonDB db = new PersonDB();
+            List<Person> obj3 = db.GetPersons();
+
+            for (int i = 0; i < obj3.Count; i++)
+            {
+                if (obj3[i].PersonID == PersonID)
+                {
+                    obj3[i].PersonID = PersonID;
+
+                    obj3[i].FirstName = person.FirstName;
+
+                    obj3[i].LastName = person.LastName;
+
+                    obj3[i].Address = person.Address;
+
+                    obj3[i].City = person.City;
+
+                }
+
+                else
+                {
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
+
+
+
+        [HttpGet]
+
+        [Route("get")]
+
+        public Person GetPersonByID(Person person,int PersonID)
+        {
+            PersonDB db = new PersonDB();
+
+            Person personselected = new Person();
+
+            List<Person> obj4 = db.GetPersons();
+
+            personselected = (from s in obj4
+                         where s.PersonID == PersonID
+                         select s).FirstOrDefault();
+
+            return personselected;
+
+            
+        }
+
+
+        [HttpPost]
+        [Route("post")]
+
+        public bool CreatePerson(Person person)
+        {
+            PersonDB db = new PersonDB();
+
+            List<Person> obj5 = db.GetPersons();
+
+            obj5.Add(person);
+
+            return true;
+        }
+
+
+    }
+}
---
+++ WebApplication6/WebApplication6/Models/PersonDB.cs
@@ -0,0 +1,57 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+
+namespace WebApplication6.Models
+{
+    public class PersonDB
+    {
+            public List<Person> GetPersons()
+            {
+                string cs = "Data Source=ABJIMA-ISHANSI; Initial Catalog=ado_db;Integrated Security=true;";
+            SqlConnection con = new SqlConnection(cs);
+
+            List<Person> PersonList = new List<Person>();
+
+                
+
+                SqlCommand cmd = new SqlCommand("GetPersons", con);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                con.Open();
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Person Person = new Person();
+
+                    Person.PersonID = Convert.ToInt32(dr.GetValue(0).ToString());
+
+                    Person.FirstName = dr.GetValue(1).ToString();
+
+                    Person.LastName = dr.GetValue(2).ToString();
+
+                    Person.Address = dr.GetValue(3).ToString();
+
+                    Person.City = dr.GetValue(4).ToString();
+
+                    PersonList.Add(Person);
+
+                }
+
+                con.Close();
+
+
+
+                return PersonList;
+
+
+            }
+
+
+        }
+
+    }
+
